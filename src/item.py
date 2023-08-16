68cc8f72372dd3d36a4b68a6012c1bec0f60e6f8
melch250 <melchoir1@yandex.ru>
2023-08-17 00:13:27 +0300

    upd1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 
 
 class Item:
@@ -10,15 +11,48 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        if len(name) <= 10:
+            self._name = name
+        else:
+            self._name = name[:10]
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        csv_items = '/Users/vlad/Desktop/Python/electronics-shop-project/src/items.csv'
+        created_items = []
+        with open(csv_items, newline='') as csvfile:
+            lines = csv.DictReader(csvfile)
+            for line in lines:
+                #if 'name' in line and 'price' in line and 'quantity' in line:
+                name = line['name'].strip()
+                price = float(line['price'].strip())
+                quantity = int(line['quantity'].strip())
+                item = cls(name, price, quantity)
+                created_items.append(item)
+        return created_items
+        #print(len(created_items))
+
+
+    @staticmethod
+    def string_to_number(value:str) -> float:
+        return float(value)
+
 
 
     def calculate_total_price(self) -> float:
